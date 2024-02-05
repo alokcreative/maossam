26b5f4bd2888dfb334682586c6fb6f30c3e06c17
alokcreative <alok@creativebuffer.com>
2024-02-05 15:17:22 +0530

    logo size

--- src/layout/Brand/Brand.tsx
+++ src/layout/Brand/Brand.tsx
@@ -18,7 +18,7 @@ const Brand: FC<IBrandProps> = ({ asideStatus, setAsideStatus }) => {
 			<div className='brand-logo'>
 				<div className='brand-title'>
 					<Link to='/' aria-label='Logo' className='d-flex'>
-						<Logo height={50} width={150} />
+						<Logo height={40} width={150} />
 					</Link>
 				</div>
 			</div>
